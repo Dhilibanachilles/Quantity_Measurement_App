229508e96bd5c17cfd47895fabdbb9100dd75c93
dhiliban <dhiliban.baskar@gmail.com>
2021-04-18 16:59:25 +0530

    Added:Temperature class to calculate TemperatureType

---
+++ app/src/main/java/com/example/quantitymeasurement/TemperatureType.java
@@ -0,0 +1,24 @@
+package com.example.quantitymeasurement;
+
+public class TemperatureType {
+    public String result = "";
+    public String calculate(int inputValue, String fromUnit, String toUnit) {
+        double a = 0.55;
+        double b = 1.8;
+
+        if(fromUnit.equals(toUnit)){
+            result = String.format("%f",inputValue);
+        }
+        else if(fromUnit.equals("Celsius") && toUnit.equals("Fahrenheit")){
+            double calculation = (inputValue*b) + 32;
+            result = String.format("%f",calculation);
+        }
+        else if(fromUnit.equals("Fahrenheit") && toUnit.equals("Celsius")){
+            double calculation = (inputValue-32) * a;
+            result = String.format("%f",calculation);
+        }
+        else{
+        }
+        return result;
+    }
+}
