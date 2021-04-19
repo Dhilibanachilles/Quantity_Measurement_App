bca6106e31268dda41bc6ea09770504305a1a44e
dhiliban <dhiliban.baskar@gmail.com>
2021-04-19 15:59:03 +0530

    Updated: TemperatureType.java file

--- app/src/main/java/com/example/quantitymeasurement/TemperatureType.java
+++ app/src/main/java/com/example/quantitymeasurement/TemperatureType.java
@@ -6,18 +6,15 @@ public class TemperatureType {
         double a = 0.55;
         double b = 1.8;
 
-        if(fromUnit.equals(toUnit)){
+        if(fromUnit.equals(toUnit)) {
             result = String.format("%f",inputValue);
-        }
-        else if(fromUnit.equals("Celsius") && toUnit.equals("Fahrenheit")){
+        } else if(fromUnit.equals("Celsius") && toUnit.equals("Fahrenheit")) {
             double calculation = (inputValue*b) + 32;
             result = String.format("%f",calculation);
-        }
-        else if(fromUnit.equals("Fahrenheit") && toUnit.equals("Celsius")){
+        } else if(fromUnit.equals("Fahrenheit") && toUnit.equals("Celsius")) {
             double calculation = (inputValue-32) * a;
             result = String.format("%f",calculation);
-        }
-        else{
+        } else{
         }
         return result;
     }
